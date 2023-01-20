26b87cc1faa3278d8badc0629686bc2b33601447
Pain <vadimklimkin1@gmail.com>
2023-01-20 15:54:23 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        int score = 100;//счет клиента (начальные данные).
+        int depositAccount = 1100; //депозитный счет клиента -счет для накопления бонусных средств.
+
+        int bonus;
+
+        if (depositAccount > 1000) {
+            bonus = depositAccount / 100;//формула подсчета бонусных рублей за каждые доп внесенные 100р на депозит.
+        } else {
+            bonus = 0;
+        }
+        int balance = score + depositAccount + bonus;
+        System.out.println("Ваш итоговый счет: " + balance + " руб.");
+        System.out.println("Ваш бонус: " + bonus + " руб.");
+    }
+}
+
